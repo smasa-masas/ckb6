f43a22f2304951845f90110412bf0b7b989a9d63
木村 光輝 <67196597+telzo2000@users.noreply.github.com>
2024-05-23 05:37:21 +0900

    fix

--- firmware/ckb6/keymaps/vial/config.h
+++
@@ -1,7 +0,0 @@
-// Copyright 2023 m.ki (@telzo2000)
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-#pragma once
-#define VIAL_KEYBOARD_UID {0x70, 0xB9, 0xAE, 0x88, 0x0B, 0xEC, 0xA2, 0x36}
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
--- firmware/ckb6/rules.mk
+++
@@ -1,9 +0,0 @@
-# This file intentionally left blank
-ENCODER_ENABLE = yes
-ENCODER_MAP_ENABLE = yes
-VIA_ENABLE = yes
-POINTING_DEVICE_ENABLE = yes
-POINTING_DEVICE_DRIVER = analog_joystick
-SRC += analog.c
-
-
